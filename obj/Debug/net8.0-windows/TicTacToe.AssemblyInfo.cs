b9042df07552aa49f256eef44efc57480bf3cdd5
Joshua Simpson <163743276+MrJSimpson@users.noreply.github.com>
2025-02-22 08:45:11 -0800

    Final test complete.

--- obj/Debug/net8.0-windows/TicTacToe.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/TicTacToe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7129071ccf700dff0ee49791474c4566073d0a26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dddd76b87f95853fd9732873f6a0275ae5a0bb70")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
